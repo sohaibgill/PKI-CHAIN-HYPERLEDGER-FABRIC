50d4ad20b8cd353ec5c828ac471494920c24baa6
sohaibgill <48293403+sohaibgill@users.noreply.github.com>
2021-09-07 19:25:57 +0500

    Add files via upload

--- invoke.js
+++ invoke.js
@@ -39,11 +39,80 @@ async function main() {
         // Get the contract from the network.
         const contract = network.getContract('pki');
 
+//======================== DATA STRUCTURES =======================
+const UserDomains = {
+    U1:{
+        revokedDomains:['LSE','UOL'],
+        revokedDomainsCRTHash:['LSEcrt','UOLcrt'],
+        verifiedDomains:['pki','ssl'],
+        unverifiedDomains:['GIKI','UMT']
+    },
+    U2:{
+        revokedDomains:['FAST','LUMS'],
+        revokedDomainsCRTHash:['FASTcrt','LUMScrt'],
+        verifiedDomains:['google','domain1'],
+        unverifiedDomains:['NUST','domain2']
+    }
+}
+const verifiedDomains = {
+    pki:{
+        commonName : 'pki',
+        crtHash : 'pkicrt',
+        UID : 'U1'
+    },
+    ssl:{
+        commonName : 'ssl',
+        crtHash : 'sslcrt',
+        UID : 'U1'
+    },
+    google:{
+        commonName : 'google',
+        crtHash : 'googlecrt',
+        UID : 'U2'
+    },
+    domain1:{
+        commonName : 'domain1',
+        crtHash : 'domain1crt',
+        UID : 'U2'
+    }
+}
+const allVerifiedDomains ={
+    pki : true,
+    google : true,
+    UET : false,
+    GIKI : false
+};
+const verifiableDomains ={
+        GIKI :{
+            commonName : 'GIKI',
+            csrHash : 'GIKIcsr',
+            verificationToken : 'GIKI_token',
+            UID : 'U1'
+        },
+        NUST :{
+            commonName : 'NUST',
+            csrHash : 'NUSTcsr',
+            verificationToken : 'NUST_token',
+            UID : 'U2'
+        }
+}
+//**************************END OF DATA STRUCTRES */
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR12', 'Dave')
-        await contract.submitTransaction('createCert', 'Cert1', 'xyz5.com', 'Lahore', 'xyz5');
-        console.log('Transaction has been submitted');
+        // await contract.submitTransaction('createCert', 'Cert1', 'xyz5.com', 'Lahore', 'xyz5');
+        // console.log('Transaction has been submitted');
+        await contract.submitTransaction('writeData','verifiedDomains',JSON.stringify(verifiedDomains));
+        console.log('verifiedDomains Pushed in blockchain');
+        await contract.submitTransaction('writeData','UserDomains',JSON.stringify(UserDomains));
+        console.log('UserDomains Pushed in blockchain');
+        await contract.submitTransaction('writeData','allVerifiedDomains',JSON.stringify(allVerifiedDomains));
+        console.log('allVerifiedDomains Pushed in blockchain');
+        await contract.submitTransaction('writeData','verifiableDomains',JSON.stringify(verifiableDomains));
+        console.log('verifiableDomains Pushed in blockchain');
+
+        let response = await contract.submitTransaction('revoke','domain1','U2')
+        console.log(response.toString('ascii'))
 
         // Disconnect from the gateway.
         await gateway.disconnect();
